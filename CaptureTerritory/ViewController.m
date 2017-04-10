577b1de1669657d19c25de6c76e38f3dabd09311
sxsasha <sxsasha@sxsashas-Mac-mini.local>
2017-04-10 13:10:04 +0300

    add pods, init basic maps functional

--- CaptureTerritory/ViewController.m
+++ CaptureTerritory/ViewController.m
@@ -8,7 +8,16 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@import GoogleMaps;
+@import GoogleMapsBase;
+@import GoogleMapsCore;
+
+@interface ViewController () <CLLocationManagerDelegate, GMSMapViewDelegate>
+
+@property (weak, nonatomic) IBOutlet GMSMapView *mapView;
+
+@property (nonatomic,strong) CLLocationManager* locManager;
+@property (strong, nonatomic) CLLocation* location;
 
 @end
 
@@ -16,6 +25,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [self initLocationManager];
+    self.mapView.delegate = self;
+   // self.mapView.delegate = self;
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -23,6 +36,44 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Work with CLLocationManager
+
+-(void) initLocationManager {
+    self.locManager = [[CLLocationManager alloc]init];
+    self.locManager.distanceFilter = 20;
+    self.locManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters;
+    self.locManager.delegate = self;
+    [self.locManager requestAlwaysAuthorization];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status {
+    if  ((status == kCLAuthorizationStatusAuthorizedWhenInUse) ||
+         (status == kCLAuthorizationStatusAuthorizedAlways)) {
+        [self.locManager startUpdatingLocation];
+        self.mapView.myLocationEnabled = YES;
+        self.mapView.settings.myLocationButton = YES;
+    }
+}
+
+
+- (void)locationManager:(CLLocationManager *)manager
+     didUpdateLocations:(NSArray<CLLocation *> *)locations {
+    
+    CLLocation *location = [locations lastObject];
+    self.location = location;
+    
+    NSTimeInterval locationAge = -[location.timestamp timeIntervalSinceNow];
+    if (locationAge > 5.0) return;
+    
+    if (location.horizontalAccuracy < 0) return;
+    
+    [self changeLocation:location];
+}
+
+- (void)changeLocation:(CLLocation*)loc {
+    
 }
 
 
